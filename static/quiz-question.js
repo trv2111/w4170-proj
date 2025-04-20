c9def70c854e5b775f13382b92a4ba138f6059f5
art3mis <moon@art3mis.pm>
2025-04-20 01:28:18 -0400

    Finish basic quiz question page generation

--- static/quiz-question.js
+++ static/quiz-question.js
@@ -1,5 +1,28 @@
+function getQuestion(id) {
+	$.ajax({
+		type: "GET",
+		url: "/get_question",
+		dataType: "json",
+		data: {"id": id},
+		success: function(result) {
+			let question = result["question"]
+
+			$( ".question" ).append(question["question"])
+			Object.entries(question["answers"]).forEach(([answerid, answer]) => {
+				$( ".question-btn-container" ).append(
+					"<a href='/quiz-answer/" + id + "/" + answerid + "' class='default-button'>" + answer[0] + "</a>"
+				)
+			});
+		},
+		error: function(request, status, error){
+                  console.log("Error");
+                  console.log(request);
+                  console.log(status);
+                  console.log(error);
+         }
+	});
+}
+
 $( function() {
 	getQuestion(questionid)
-
-	// handle button presses
 })
