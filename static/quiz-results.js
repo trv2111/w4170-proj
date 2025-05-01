81701dac68a78f18fb60d808dffd0716557c57d6
art3mis <moon@art3mis.pm>
2025-05-01 00:00:31 -0400

    Fix quiz results styling

--- static/quiz-results.js
+++ static/quiz-results.js
@@ -6,10 +6,23 @@ function getScore() {
 		success: function(result) {
 			$( ".score" ).append(result["score"] + "/" + result["total"])
 
-			if (result["score"] == result["total"])
-				$( ".results-btn-container" ).append("<a href='/quiz-question/1' class='default-button'>quiz again!</a>")
-			else
-				$( ".results-btn-container" ).append("<a href='/learn' class='default-button'>review</a>")
+			if (result["score"] == result["total"]) {
+				$( ".results-text" ).append(`<b>Congratulations! You scored 100%!</b><br><br>
+					You have successfully learned how to think like a coach.<br>
+					You understand when to for it, kick it, or call a timeout – and that makes you a strategic powerhouse.
+				`)
+				$( ".results-btn-container" ).append("<a href='/quiz-question/1' class='default-button'>Quiz again!</a>")
+			} else if (result["score"] == result["total"] - 1) {
+				$( ".results-text" ).append(`<b>You're getting there!</b><br><br>
+					You have started to learn how to think like a coach, and are going in the right direction.<br>
+					Practice makes perfect! Take some time to revisit the lesson and then try again.`)
+				$( ".results-btn-container" ).append("<a href='/learn' class='default-button'>Review</a>")
+			} else {
+				$( ".results-text" ).append(`<b>Not quite there yet.</b><br><br>
+					You scored 0/3 on this quiz, but don't worry!<br>
+					Learning to think like a coach takes practice. Let's revisit the lesson and sharpen your game-time decisions.`)
+				$( ".results-btn-container" ).append("<a href='/learn' class='default-button'>Review</a>")
+			}
 		},
 		error: function(request, status, error){
                   console.log("Error");
